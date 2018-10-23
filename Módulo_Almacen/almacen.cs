1cafa467c09fab3cf37c0d7bb765fdf7ce0799fe
csanchezsalas <csanchezsalas@outlook.com>
2018-10-23 11:43:08 -0600

    Preliminary Data Structure

--- Módulo_Almacen/almacen.cs
+++ Módulo_Almacen/almacen.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Collections.Generic;
 public class Almacen{
 
     /*
@@ -9,13 +9,67 @@ public class Almacen{
     Así mismo debe almacenar referencias a métodos que deben llevar la "dirección" en el código que tiene dicho método.
      */
 
+     // PRELIMINARY DATA STRUCTURE:
+    Dictionary<string, dynamic> test;
+
+    // -----------------------------
     public String nombre;
 
     // constructor
     public Almacen(String nombre)
     {
+        this.test = new Dictionary<string, dynamic>();
         this.nombre = nombre;
     }
+
+    // ADD VALUE TO THE DICTIONARY
+    public void setValue(string key, dynamic value){
+        this.test.Add(key, value);
+    }
+    // PRINT VALUE
+    public void getValue(string key){
+        foreach (dynamic item in this.test)
+            {
+                if (item.Key == key ){
+                    Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+                }
+                
+            }
+    }
+
+    // GET VALUE IF FOUND
+    public bool searchValue(string key){
+
+        if(this.test.ContainsKey(key)){
+            Console.WriteLine(key + " ITEM FOUND");
+            return true;
+        }
+        return false;
+        
+    }
+
+    // UPDATE VALUE IF FOUND
+    public void updateValue(string key, dynamic newValue){
+        bool found = searchValue(key);
+        if(!found){
+            Console.WriteLine("404 Item not found!");
+        }
+        else{
+            this.test[key] = newValue;
+            Console.WriteLine(key + " ITEM UPDATED!");
+          
+        }
+    }
+
+    // PRINT DICTIONARY
+    public void printContainer(){
+        foreach (dynamic item in this.test)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", item.Key, item.Value);
+            }
+    }
+
+    
 
     public void almacenar(){}
 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace Minics.exe
 {
     class Program
@@ -9,6 +10,17 @@ namespace Minics.exe
             // dotnet run: correr c#
             Almacen almacen = new Almacen("almacen");
             Console.WriteLine(almacen.nombre);
+         
+        // TESTING PRELIMINARY DATA STRUCTURE:
+            almacen.setValue("KEY 1", 1);
+            almacen.setValue("KEY 2", 'a');
+            almacen.setValue("KEY 3", 2);
+            almacen.setValue("KEY 4", 'b');
+            almacen.printContainer();
+            almacen.searchValue("KEY 2");
+            almacen.updateValue("KEY 2", 666);
+            almacen.getValue("KEY 2");
+        // ------------------------------------
         }
     }
 }
