2291901752e287473eefbe847004f07e691ee013
pamVarela <varelapamela6@gmail.com>
2018-10-25 23:54:15 -0600

    se agregaron metodos en la pila

---
+++ moduloPila/pila.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+
+namespace moduloPila
+{
+    public class Pila
+    {
+
+        Stack pila = new Stack ();
+
+        public void push (Object obj){
+            pila.Push(obj);
+        }
+
+        public void pop(Object obj){
+            pila.Pop ();
+        }
+
+       public void imprimir () {
+        foreach ( Object obj in pila) {
+        Console.Write( "    {0}", obj );
+        }
+       }
+       
+         
+
+
+            
+    }
+}
